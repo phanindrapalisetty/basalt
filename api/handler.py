8db1649883d762df90ed29de152f55b3ddd38f22
phanindrapalisetty <phanindra562@gmail.com>
2026-01-12 21:12:46 +0530

    change the response type in handler

--- api/handler.py
+++ api/handler.py
@@ -21,6 +21,7 @@ async def generate(pay_load:dict):
     try:
         validate = SpecValidator.validate(pay_load)
         output = generate_dataset(pay_load)
-        return {"statuscode": 200, "message": "I'm Pikachu!!!", "data":output}
+        # return {"statuscode": 200, "message": "I'm Pikachu!!!", "data":output}
+        return JSONResponse(content={"data": output}, status_code=200)
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
